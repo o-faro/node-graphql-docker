d1af47888c0bea4ee9424fc375a2f0dea4300b5e
Oliver Faro <office@Levono-ThinkPad>
2023-07-21 13:16:35 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const { ApolloServer } = require('apollo-server');
+const { typeDefs } = require('./schema')
+const { resolvers } = require('./resolvers')
+
+const port = process.env.PORT || 9090;
+const server = new ApolloServer({ resolvers, typeDefs })
+
+server.listen({ port }, () => console.log(`Server runts at http://localhost:${port}`))
---
+++ src/schema.js
@@ -0,0 +1,24 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Student {
+    id: ID!
+    email: String!
+    fullName: String!
+    dept: String
+    enrolled: Boolean
+  }
+  type Query {
+    enrollment: [Student!]
+    students: [Student!]
+    student(id: ID!): Student
+  }
+  type Mutation {
+    registerStudent(email: String!, fullName: String!): Student! 
+    enroll(id: ID): Student
+  }
+`
+
+module.exports = {
+  typeDefs,
+}
